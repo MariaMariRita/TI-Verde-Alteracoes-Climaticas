c4238cc5ef48781bbff2e4ed0172294e9ec70a49
MariMariRita <mariaritalimadesousa11@gmail.com>
2024-12-11 18:07:38 -0300

    Base feita

--- meu_site.py
+++ meu_site.py
@@ -10,5 +10,9 @@ def homepage():
 def contatos():
     return render_template("contatos.html")
 
+@app.route("/usuario/<nome_usuario>")
+def usuarios(nome_usuario):
+    return render_template("usuarios.html", nome_usuario = nome_usuario)
+
 if __name__ == "__main__":
     app.run(debug= True )
